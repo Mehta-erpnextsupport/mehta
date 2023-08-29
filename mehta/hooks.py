53fcdb53d2cdbb27facbb746d991850073f51636
Mehta-erpnextsupport <erpnextsupport@mehtasoftech.com>
2023-08-29 05:23:38 +0000

    commit data

--- mehta/hooks.py
+++ mehta/hooks.py
@@ -48,6 +48,8 @@ fixtures = [
 doctype_js = {
     "Journal Entry" : "public/js/journal_entry.js",
 }
+
+
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
